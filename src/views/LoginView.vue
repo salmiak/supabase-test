6ec403486c370f5be234ab979cd1c2facc321fb4
Alfred Beckman <alfred.beckman@prorenata.se>
2025-05-05 22:25:29 +0200

    Fix login page page to take any email-address

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,15 +3,16 @@ import { ref } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 
 const status = ref('Klicka på knappen för att skicka mailet')
+const email = ref('')
 
 const signInWithEmail = async () => {
   status.value = 'skickar...'
   const { data, error } = await supabase.auth.signInWithOtp({
-    email: 'beckmanalfred@gmail.com',
+    email: email.value,
     options: {
       // set this to false if you do not want the user to be automatically signed up
       shouldCreateUser: false,
-      emailRedirectTo: 'https://example.com/welcome',
+      emailRedirectTo: 'https://salmiak.github.io/supabase-test/',
     },
   })
   status.value = 'Skickat - kolla din mail'
@@ -20,6 +21,11 @@ const signInWithEmail = async () => {
 
 <template>
   <h2>Logga in</h2>
+  <input
+    type="email"
+    placeholder="Skriv din e-postadress"
+    class="border border-teal-300 bg-teal-100 rounded-md p-2 mb-4"
+    v-model="email" /><br />
   <button @click="signInWithEmail">Skicka magiskt mejl</button><br />
   {{ status }}
 </template>
--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/supabase-test/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/supabase-test/' : '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
